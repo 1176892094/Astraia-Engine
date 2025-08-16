ecd7b67bfbee2238dccf8429ba00f9450e1753a4
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-16 22:15:10 +0800

    渲染api

--- Runtime/Source/Core/Application.h
+++ Runtime/Source/Core/Application.h
@@ -5,6 +5,7 @@
 #include "Source/Events/Event.h"
 #include "Source/Events/ApplicationEvent.h"
 #include "Source/GUI/ImGuiLayer.h"
+#include "Source/Renderer/Buffer.h"
 
 namespace Engine
 {
@@ -33,8 +34,10 @@ namespace Engine
         bool m_Running = true;
         LayerStack m_LayerStack;
 
-        unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+        unsigned int m_VertexArray;
         std::unique_ptr<Shader> m_Shader;
+        std::unique_ptr<VertexBuffer> m_VertexBuffer;
+        std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
         static Application* s_Instance;
     };
