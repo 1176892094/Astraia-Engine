6e0391ad770844b1257a60e7bd2f3921f7b71b0e
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-16 15:19:06 +0800

    spdlog

--- Runtime/Engine.h
+++ Runtime/Engine.h
@@ -1,9 +1,9 @@
 #pragma once
 
-// For use by Hazel applications
-
 #include "Source/Core/Application.h"
 
 // ---Entry Point---------------------
 #include "Source/Core/EntryPoint.h"
+
+#include "Source/Core/Log.h"
 // -----------------------------------
