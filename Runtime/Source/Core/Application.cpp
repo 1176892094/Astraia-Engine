24b4bc24a6b86d5c6430f3b5f86bf7c38fcfe971
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-16 23:21:54 +0800

    RendererAPI封装

--- Runtime/Source/Core/Application.cpp
+++ Runtime/Source/Core/Application.cpp
@@ -1,8 +1,8 @@
 #pragma once
 #include "Header.h"
-#include <glad/glad.h>
 #include "Application.h"
 #include "Log.h"
+#include "Source/Renderer/Renderer.h"
 
 
 namespace Engine
@@ -157,16 +157,18 @@ namespace Engine
     {
         while (m_Running)
         {
-            glClearColor(0.1, 0.1, 0.1, 1);
-            glClear(GL_COLOR_BUFFER_BIT);
+        	RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+        	RenderCommand::Clear();
+
+        	Renderer::BeginScene();
 
         	m_BlueShader->Bind();
-        	m_SquareVA->Bind();
-        	glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+        	Renderer::Submit(m_SquareVA);
 
             m_Shader->Bind();
-        	m_VertexArray->Bind();
-        	glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+        	Renderer::Submit(m_VertexArray);
+
+        	Renderer::EndScene();
 
             for (Layer *layer: m_LayerStack)
             {
