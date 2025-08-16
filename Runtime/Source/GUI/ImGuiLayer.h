051745fb04101dc723396c7791f8f542dd70b9c7
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-16 18:12:33 +0800

    MouseEvent

--- Runtime/Source/GUI/ImGuiLayer.h
+++ Runtime/Source/GUI/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Source/Core/Layer.h"
+#include "Source/Events/ApplicationEvent.h"
+#include "Source/Events/KeyEvent.h"
+#include "Source/Events/MouseEvent.h"
 
 namespace Engine
 {
@@ -18,7 +21,15 @@ namespace Engine
         void OnUpdate();
 
         void OnEvent(Event &event);
-
+    private:
+        bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+        bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+        bool OnMouseMovedEvent(MouseMovedEvent& e);
+        bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+        bool OnKeyPressedEvent(KeyPressedEvent& e);
+        bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+        bool OnKeyTypedEvent(KeyTypedEvent& e);
+        bool OnWindowResizeEvent(WindowResizeEvent& e);
     private:
         float m_Time = 0.0f;
     };
