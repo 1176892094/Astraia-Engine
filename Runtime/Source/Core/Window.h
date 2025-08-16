5098fe7b13f914d73bc649cb13d7248b33b6e38a
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-16 20:54:19 +0800

    Update Window.h

--- Runtime/Source/Core/Window.h
+++ Runtime/Source/Core/Window.h
@@ -11,7 +11,7 @@ namespace Engine
         unsigned int Width;
         unsigned int Height;
 
-        WindowProps(const std::string &title = "Hazel Engine", unsigned int width = 1280, unsigned int height = 720) : Title(title), Width(width), Height(height)
+        WindowProps(const std::string &title = "Astraia Engine", unsigned int width = 1280, unsigned int height = 720) : Title(title), Width(width), Height(height)
         {
         }
     };
