fd11df805f8f361c31b12e0ff728f1c71ecdb32c
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-17 20:55:37 +0800

    RendererId

--- Runtime/Source/Core/Application.cpp
+++ Runtime/Source/Core/Application.cpp
@@ -47,6 +47,11 @@ namespace Engine
         m_LayerStack.PushOverlay(layer);
     }
 
+    void Application::Close()
+    {
+        m_Running = false;
+    }
+
     void Application::OnEvent(Event &e)
     {
         HZ_PROFILE_FUNCTION();
--- Runtime/Source/Core/Application.h
+++ Runtime/Source/Core/Application.h
@@ -6,7 +6,7 @@
 #include "Source/Events/ApplicationEvent.h"
 #include "Source/GUI/ImGuiLayer.h"
 
-int main(int argc, char** argv);
+int main(int argc, char **argv);
 
 namespace Engine
 {
@@ -17,33 +17,41 @@ namespace Engine
 
         virtual ~Application();
 
-        void OnEvent(Event& e);
+        void OnEvent(Event &e);
 
-        void PushLayer(Layer* layer);
-        void PushOverlay(Layer* layer);
+        void PushLayer(Layer *layer);
 
-        Window& GetWindow() { return *m_Window; }
+        void PushOverlay(Layer *layer);
 
-        static Application& Get() { return *s_Instance; }
+        Window &GetWindow()
+        {
+            return *m_Window;
+        }
 
+        void Close();
+
+        static Application &Get()
+        {
+            return *s_Instance;
+        }
 
     private:
         void Run();
 
-        bool OnWindowClose(WindowCloseEvent& e);
+        bool OnWindowClose(WindowCloseEvent &e);
 
-        bool OnWindowResize(WindowResizeEvent& e);
+        bool OnWindowResize(WindowResizeEvent &e);
 
         std::unique_ptr<Window> m_Window;
-        ImGuiLayer* m_ImGuiLayer;
+        ImGuiLayer *m_ImGuiLayer;
         bool m_Running = true;
         bool m_Minimized = false;
         LayerStack m_LayerStack;
         float m_LastFrameTime = 0.0f;
 
-        static Application* s_Instance;
+        static Application *s_Instance;
 
-        friend int ::main(int argc, char** argv);
+        friend int ::main(int argc, char **argv);
     };
 
     Application *CreateApplication();
