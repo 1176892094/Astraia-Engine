1e49114a5faf8f0743881abb28b0b16e84a3a051
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-18 02:49:48 +0800

    引擎皮肤

--- Runtime/Source/GUI/ImGuiLayer.h
+++ Runtime/Source/GUI/ImGuiLayer.h
@@ -26,6 +26,8 @@ namespace Engine
             m_BlockEvents = block;
         }
 
+        void SetDarkThemeColors();
+
     private:
         bool m_BlockEvents = true;
         float m_Time = 0.0f;
