a383d55ef7d1b70bae09d1bcb420add60e291cb8
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-18 01:32:06 +0800

    支持虚函数

--- Runtime/Source/Scene/ScriptableEntity.h
+++ Runtime/Source/Scene/ScriptableEntity.h
@@ -7,10 +7,27 @@ namespace Engine
     class ScriptableEntity
     {
     public:
+        virtual ~ScriptableEntity()
+        {
+        }
+
         template<typename T>
         T &GetComponent()
         {
             return m_Entity.GetComponent<T>();
+        }
+
+    protected:
+        virtual void OnCreate()
+        {
+        }
+
+        virtual void OnDestroy()
+        {
+        }
+
+        virtual void OnUpdate(Timestep ts)
+        {
         }
 
     private:
