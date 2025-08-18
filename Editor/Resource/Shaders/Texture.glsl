814165c7228d9493776c437a2951287841788acd
Charlotte <75679851+1176892094@users.noreply.github.com>
2025-08-18 19:22:50 +0800

    鼠标帧缓冲区

--- Editor/Resource/Shaders/Texture.glsl
+++ Editor/Resource/Shaders/Texture.glsl
@@ -29,6 +29,7 @@ gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 #version 330 core
 
 layout(location = 0) out vec4 color;
+layout(location = 1) out int color2;
 
 in vec4 v_Color;
 in vec2 v_TexCoord;
@@ -60,4 +61,5 @@ case 14: texColor *= texture(u_Textures[14], v_TexCoord * v_TilingFactor); break
 case 15: texColor *= texture(u_Textures[15], v_TexCoord * v_TilingFactor); break;
 }
 color = texColor;
+color2 = 50;
 }
